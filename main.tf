bc508b8318da866cfd4d50eb2a1e532569d8e71a
mark tran <mark.tran@hotmail.co.uk>
2017-06-07 14:11:11 +0100

    added IAM for S3 access

--- main.tf
+++ main.tf
@@ -211,3 +211,50 @@ resource "aws_key_pair" "deployer" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
+
+#IAM
+resource "aws_iam_role_policy" "s3_policy" {
+  name = "s3_access_policy"
+  role = "${aws_iam_role.s3_access.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "s3_access" {
+  name  = "s3_access"
+  role = "${aws_iam_role.s3_access.name}"
+}
+
+resource "aws_iam_role" "s3_access" {
+  name = "s3_access_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
