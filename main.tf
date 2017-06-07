ec4223937707c14cef479eaffacc4ebf7c23966f
mark tran <mark.tran@hotmail.co.uk>
2017-06-07 09:02:07 +0100

    added public, private and rds security groups

--- main.tf
+++ main.tf
@@ -135,11 +135,59 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 resource "aws_security_group" "public" {
   name        = "public_sg"
   description = "Used for public and private instances for load balancer access"
+  vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["${var.localip}"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "private_sg"
+  description = "Used for private instances"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.vpc_cidr_block}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds_sg"
+  description = "Used for DB instances"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.public.id}", "${aws_security_group.private.id}"]
   }
 }
